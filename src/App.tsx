51d80cf65bd97c9749d5ea66286a19cdbfd024d5
lucas <lucasgabrielftp17@gmail.com>
2021-06-19 09:43:32 -0300

    primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import api from "./api/api";
 
 function App() {
+  async function loadProducts() {
+    const productsList = await api.get("produtos/all");
+    setProducts(productsList.data);
+  }
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
+  const [products, setProducts] = useState([
+    { idProduto: 0, codProduto: 0, descricao: "" },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {products.length === 0 ? (
+          <span>Nada registrado</span>
+        ) : (
+          products.map((product) => (
+            <li
+              key={product.idProduto}
+              style={{ textDecoration: "none", listStyleType: "none" }}
+            >
+              {product.codProduto} {"-->"} {product.descricao}
+            </li>
+          ))
+        )}
+      </ul>
     </div>
   );
 }
